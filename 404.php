17dde2ff325331691473d462d4a44ad0d7397bc0
tomtomgo92 <tm.lepoleswebforce3@gmail.com>
2015-10-15 09:54:05 +0200

    Page 404 & programmation de la page

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr_FR">
+<head>
+      <meta charset="UTF-8">
+      <title>404 - episodes</title>
+      <!-- Slider header -->
+      <link rel="stylesheet" href="cssslider_files/csss_engine1/style.css">
+      <!-- Favicon -->
+      <link rel="icon" href="images/logo_icon.png" type="image/png">
+      <!-- Hover.css -->
+      <link rel="stylesheet" href="Hover-master/css/hover.css">
+      <!--  CSS -->
+      <link rel="stylesheet" type="text/css" href="css/normalize.css">
+      <link rel="stylesheet" type="text/css" href="css/accueil.css">
+      <link rel="stylesheet" type="text/css" href="css/connect.css">
+      <!-- Font -->
+       <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" type="text/css" href="font-awesome-4.3.0/css/font-awesome.css">
+      <!-- Jquery -->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <!--[if IE]>
+      <script type="text/javascript" src="js/modernizr.custom.78869.js"></script>
+      <![endif]-->
+
+</head>
+<body>
+	      <?php 
+         include('includes/db.php');
+         include("header.php");
+               ?>
+    <h2>Erreur 404</h2>
+    <p>Les chercheurs, excédés d’aller sans cesse relancer un serveur défaillant installé dans le bureau n°404, aient attribué ce numéro d’erreur au défaut de connexion, en souvenir de cette pièce maudite.</p>
+	<img src="images/giphy.gif">
+</body>
+</html>
